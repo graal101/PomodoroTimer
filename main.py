0d0e21c4cb42ff04e60d16238a79823cf4ae8d18
Andrew Andrey <graal101@mail.ru>
2025-12-20 12:17:54 +0700

    added app ico to the trey

--- main.py
+++ main.py
@@ -1,41 +1,46 @@
 #! /usr/bin/env python3
 import sys
-import time
 from libs.sound import Sound
 from Dialogs.dialogs import FileDialog
 from PyQt6.QtCore import QTimer
 from PyQt6 import uic
-from PyQt6.QtWidgets import QApplication, QMainWindow, QMessageBox
+from PyQt6.QtWidgets import QApplication, QMainWindow, QSystemTrayIcon
+from PyQt6.QtGui import QIcon
+
 
 # FIX documentation! Linter it!
 class PauseState():
-    '''Класс для фиксации времен при паузе.'''
+    """Класс для фиксации времен при паузе."""
+
     def __init__(self):
         self.t_work = 0
         self.t_rest = 0
         self.edit = True  # Флаг для переключения между режимами, True = work
-        
+
     def start_state(self):
         self.t_work = 0
         self.t_rest = 0
         self.edit = True
-        
-        
+      
+
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         # Загружаем пользовательский интерфейс из файла .ui
-        uic.loadUi("ui/main_window.ui", self)
+        uic.loadUi('ui/main_window.ui', self)
         self.btn_start.clicked.connect(self.on_btn_start_click)
         self.btn_stop.clicked.connect(self.on_btn_stop)
         self.btn_pause.clicked.connect(self.on_btn_pause)
         self.mn_quit.triggered.connect(self.app_quit)
         self.mn_font.triggered.connect(self.app_font)
-        
+        self.tray_icon = QSystemTrayIcon(self)
+        self.tray_icon.setIcon(QIcon("ico/rotten-tomatoes-logo.png"))
         self.timer = QTimer()
         self.timer.timeout.connect(self.update_timer)
         self.remaining_time = 0
         
+        self.tray_icon.show()
+
     def start_countdown(self, t: int):
         print(p.t_work)
         if p.t_work == 0:
@@ -46,7 +51,7 @@ class MainWindow(QMainWindow):
         self.Bar_work.setMaximum(t)
         self.Bar_work.setValue(0)
         self.timer.start(1000)  # запускает таймер с интервалом 1 секунда
-        
+
     def update_timer(self):
         if self.remaining_time > 0:
             mins, secs = divmod(self.remaining_time, 60)
@@ -56,34 +61,26 @@ class MainWindow(QMainWindow):
             self.remaining_time -= 1
             p.t_work = self.remaining_time
             print(p.t_work)
-
         else:
             self.timer.stop()  # останавливает таймер, когда время истекает
             p.start_state()
-            snd = Sound(1200, 1000)
-            snd.play_sound()
-            snd = Sound(1200, 1000)
-            snd.play_sound()
-            snd = Sound(1200, 1000)
-            snd.play_sound()
-        
+            self.sound_mod('stop')
 
     def on_btn_start_click(self):
-        snd = Sound(1200, 2500)
-        snd.play_sound()
+        self.sound_mod('start')
         w = self.spin_work.value() * 60  # Время задания
         r = self.spin_rest.value() * 60  # Время перерыва
         self.start_countdown(w if p.edit else r)
-        
+
     def on_btn_stop(self):
         print('stop')
         self.timer.stop()
         p.start_state()
-        
+
     def on_btn_pause(self):
         print('paused')
         self.timer.stop()
-        
+
     def app_font(self):
         font_open = FileDialog()
         fsize = font_open.font_dialog()
@@ -92,11 +89,23 @@ class MainWindow(QMainWindow):
             self.label_info.setFont(fsize)
             self.label_time.setFont(fsize)
 
+    def sound_mod(self, mode):
+        if mode == 'stop':
+            snd = Sound(1200, 1000)
+            snd.play_sound()
+            snd = Sound(1200, 1000)
+            snd.play_sound()
+            snd = Sound(1200, 1000)
+            snd.play_sound()
+        if mode == 'start':
+            snd = Sound(1200, 2500)
+            snd.play_sound()
         
     def app_quit(self):
         quit()
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     p = PauseState()
     app = QApplication(sys.argv)
     window = MainWindow()
